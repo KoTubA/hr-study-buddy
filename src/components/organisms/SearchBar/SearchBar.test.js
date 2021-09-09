27c271668190d10574aafc14c1b4ece61d6d6e36
KoTubA <szymon.kotarba988@gmail.com>
2021-09-09 15:42:00 +0200

    Add missing test for Searchbar and refactor render method in all tests

--- src/components/organisms/SearchBar/SearchBar.test.js
+++ src/components/organisms/SearchBar/SearchBar.test.js
@@ -1,4 +1,5 @@
-import { render, screen, fireEvent } from 'test-utils';
+import '@testing-library/jest-dom';
+import { render, screen, fireEvent, waitFor } from 'test-utils';
 import { setupServer } from 'msw/node';
 import { handlers } from 'mocks/handlers';
 import { SearchBar } from './SearchBar';
@@ -22,5 +23,17 @@ describe('Search Bar', () => {
     fireEvent.change(input, { target: { value: 'ad' } });
 
     await screen.findByText(/Adam Romański/);
+  });
+
+  it('Hides the results when input is empty', async () => {
+    render(<SearchBar />);
+    const input = screen.getByPlaceholderText('Search');
+    fireEvent.change(input, { target: { value: 'ad' } });
+    await screen.findByText(/Adam Romański/);
+
+    fireEvent.change(input, { target: { value: '' } });
+    await waitFor(() => {
+      expect(screen.getByLabelText('results')).not.toBeVisible();
+    });
   });
 });
