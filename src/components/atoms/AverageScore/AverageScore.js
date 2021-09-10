ababb83e70619e8068fc02dc335e42e507cb1e08
KoTubA <szymon.kotarba988@gmail.com>
2021-09-11 00:14:34 +0200

    Add react modal

--- src/components/atoms/AverageScore/AverageScore.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { StyledAverageScore } from './AverageScore.styles';
-
-export const AverageScore = ({ average }) => <StyledAverageScore value={average}>{average}</StyledAverageScore>;
