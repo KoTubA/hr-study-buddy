c886489cbb747b99cd490447dd993cbf897d5114
KoTubA <szymon.kotarba988@gmail.com>
2021-09-15 23:59:55 +0200

    Add integration-test to Root component

--- src/components/templates/NewsSection/NewsSection.test.js
+++ src/components/templates/NewsSection/NewsSection.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
-import { renderWithProviders } from 'helpers/renderWithProviders';
+import { render, screen } from 'test-utils';
 import NewsSection from 'components/templates/NewsSection/NewsSection';
 
 const mockResponse = {
@@ -16,22 +15,18 @@ describe('News Section', () => {
 
   it('Displays error when the article are not loaded correctly', async () => {
     jest.spyOn(global, 'fetch').mockImplementationOnce(() => Promise.reject('Async error'));
-    renderWithProviders(<NewsSection />);
+    render(<NewsSection />);
     await screen.findAllByText(/Sorry/);
   });
 
   it('Displays the articles', async () => {
     jest.spyOn(global, 'fetch').mockImplementationOnce(() =>
       Promise.resolve({
-        json: () =>
-          Promise.resolve({
-            data: {
-              allArticles: [{ id: 1, title: 'Test', category: 'Test', content: 'Test' }],
-            },
-          }),
+        ok: true,
+        json: () => Promise.resolve(mockResponse),
       })
     );
-    renderWithProviders(<NewsSection />);
+    render(<NewsSection />);
     await screen.findAllByText(/Test/);
   });
 });
